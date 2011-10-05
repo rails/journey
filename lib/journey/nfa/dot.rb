c7444ef0f9c80a84fc2c829755cc001433786714
Aaron Patterson <aaron.patterson@gmail.com>
2011-10-04 18:15:48 -0700

    adding clustering to the dot graph

--- lib/journey/nfa/dot.rb
+++ lib/journey/nfa/dot.rb
@@ -5,20 +5,32 @@ module Journey
     module Dot
       def to_dot
         edges = transitions.map { |from, sym, to|
-          "#{from} -> #{to} [label=\"#{sym || 'ε'}\"];"
+          "    #{from} -> #{to} [label=\"#{sym || 'ε'}\"];"
         }
 
-        nodes = states.map { |i| "#{i} [label=\"#{i}\"];" }
+        memo_nodes = memos.values.flatten.map { |n|
+          "  #{n.object_id} [label=\"#{n}\", color=1, style=filled, shape=note];"
+        }
+        memo_edges = memos.map { |k, memos|
+          memos.map { |v| "  #{k} -> #{v.object_id} [color=5];" }
+        }.flatten
 
         <<-eodot
 digraph nfa {
   rankdir=LR;
-  size="8,5"
-  node [shape = doublecircle];
-  #{accepting};
-  node [shape = circle];
-  #{nodes.join "\n"}
-  #{edges.join "\n"}
+  edge [colorscheme=paired7];
+  node [colorscheme=paired7];
+  subgraph cluster_fsm {
+    color=blue;
+    label="State Machine";
+    node [shape = doublecircle];
+    #{accepting_states.join ' '};
+    node [shape = circle];
+#{edges.join "\n"}
+  }
+
+#{memo_nodes.join "\n"}
+#{memo_edges.join "\n"}
 }
         eodot
       end
